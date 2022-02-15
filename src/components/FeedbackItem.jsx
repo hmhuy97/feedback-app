53b9518b53518383fdddc12737dd1e1decb00490
Huỳnh Minh Huy <39719651+hmhuy97@users.noreply.github.com>
2022-02-15 14:14:33 +0700

    init

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,10 @@
+function FeedbackItem() {
+  return (
+    <div className="card">
+      <div className="num-display">10</div>
+      <div className="text-display">This is an example ofa feedback</div>
+    </div>
+  );
+}
+
+export default FeedbackItem;
