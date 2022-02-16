6d3ef1c8e5017f4287f063652f9d1becc23eedbe
Huỳnh Minh Huy <39719651+hmhuy97@users.noreply.github.com>
2022-02-16 17:17:01 +0700

    ghh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import "./App.css";
+import { useState } from "react";
 import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+import Card from "./components/shared/Card";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    console.log("APP", id);
+  };
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} hanldeDelete={deleteFeedback} />
       </div>
     </>
   );
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "ipsum dolor sit amet consectetur adipisicing elit. Placeat, vero voluptas, officia id corporis, inventore error voluptate aliquid ut ex tenetur quam aspernatur? Culpa blanditiis ullam architecto iste, voluptas voluptatem?",
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: "ipsum dolor sit amet consectetur adipisicing elit. Placeat, vero voluptas, officia id corporis, inventore error voluptate aliquid ut ex tenetur quam aspernatur? Culpa blanditiis ullam architecto iste, voluptas voluptatem?",
+  },
+  {
+    id: 3,
+    rating: 2,
+    text: "ipsum dolor sit amet consectetur adipisicing elit. Placeat, vero voluptas, officia id corporis, inventore error voluptate aliquid ut ex tenetur quam aspernatur? Culpa blanditiis ullam architecto iste, voluptas voluptatem?",
+  },
+  {
+    id: 4,
+    rating: 8,
+    text: "ipsum dolor sit amet consectetur adipisicing elit. Placeat, vero voluptas, officia id corporis, inventore error voluptate aliquid ut ex tenetur quam aspernatur? Culpa blanditiis ullam architecto iste, voluptas voluptatem?",
+  },
+];
+
+export default FeedbackData;
