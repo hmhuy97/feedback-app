3d2a9d453ce5b4973852b2f7d55813dbed193f5d
Huỳnh Minh Huy <39719651+hmhuy97@users.noreply.github.com>
2022-02-22 17:26:39 +0700

    clip 4 rating select

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import Card from "./components/shared/Card";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  // const deleteFeedback = (id) => {
-  //   // console.log("APP", id);
-  //   if(window.confirm("Are you sure you want to delete"))
-  // };
+  const deleteFeedback = (id) => {
+    // console.log("APP", id);
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} hanldeDelete={deleteFeedback} />
       </div>
     </>
