71286d2267466850894544aed40459b82a38e8c5
Huỳnh Minh Huy <39719651+hmhuy97@users.noreply.github.com>
2022-03-23 15:33:52 +0700

    done clip 3 f6

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+// import FeedbackItem from "./components/FeedbackItem";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
+// import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
 import { FeedbackProvider } from "./context/FeedbackContext";
-import Card from "./components/shared/Card";
-import Post from "./components/Post";
+// import Card from "./components/shared/Card";
+// import Post from "./components/Post";
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-  const deleteFeedback = (id) => {
-    // console.log("APP", id);
-    if (window.confirm("Are you sure you want to delete")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
   return (
     <FeedbackProvider>
       <Router>
@@ -35,12 +22,9 @@ function App() {
               path="/"
               element={
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackForm />
                   <FeedbackStats />
-                  <FeedbackList
-                    feedback={feedback}
-                    hanldeDelete={deleteFeedback}
-                  />
+                  <FeedbackList />
                   <AboutIconLink />
                 </>
               }
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
-import PropTypes from "prop-types";
 import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
-function FeedbackList({ hanldeDelete }) {
-  const {feedback} = useContext(FeedbackContext)
+
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext);
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
@@ -19,11 +19,7 @@ function FeedbackList({ hanldeDelete }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-              handleDelete={hanldeDelete}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
