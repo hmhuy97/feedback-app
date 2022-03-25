5c2fa121921c4e1cae58caeefadd4bcd7e5a8f78
Huỳnh Minh Huy <39719651+hmhuy97@users.noreply.github.com>
2022-03-25 09:54:56 +0700

    done clip7. Update & Delete From JSON-Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -37,17 +37,25 @@ export const FeedbackProvider = ({ children }) => {
     setFeedbackEdit({ item, edit: true });
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     // console.log("APP", id);
+
     if (window.confirm("Are you sure you want to delete")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updItem),
+    });
+    const data = await response.json();
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
